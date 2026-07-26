48fad0b32bd93784aae07607bfeea451168840d1
SankalpVerma31 <mrblackcap31@gmail.com>
2026-07-26 11:27:23 +0530

    Time: 0 ms (100%), Space: 9.2 MB (51.03%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,18 @@
+//Sankalp
+#include <limits.h>
+
+int reverse(int x) {
+    int reverse = 0;
+    
+    while (x != 0) {
+        int remainder = x % 10;
+        if (reverse > INT_MAX / 10 || reverse < INT_MIN / 10) {
+            return 0;
+        }
+        
+        reverse = (reverse * 10) + remainder;
+        x = x / 10;
+    }
+    
+    return reverse;
+}
