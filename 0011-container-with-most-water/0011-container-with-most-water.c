5dd64eeb667f22aa656969c46dc2f04aaad5b41e
SankalpVerma31 <mrblackcap31@gmail.com>
2026-07-26 11:28:59 +0530

    Time: 7 ms (5.13%), Space: 14.6 MB (84.89%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -0,0 +1,22 @@
+//Sankalp
+int maxArea(int* height, int heightSize) {
+    int max_area = 0;
+    int left = 0;
+    int right = heightSize - 1;
+
+    while (left < right) {
+        int width = right - left;
+        int current_height = (height[left] < height[right]) ? height[left] : height[right];
+        int current_area = current_height * width;
+        if (current_area > max_area) {
+            max_area = current_area;
+        }
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max_area;
+}
