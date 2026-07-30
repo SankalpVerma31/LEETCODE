c05c55d002794bc9cf92f15492d36cda302a77f2
SankalpVerma31 <mrblackcap31@gmail.com>
2026-07-30 20:15:59 +0530

    Time: 12 ms (1.54%), Space: 77.4 MB (19.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,56 +4,39 @@ class Solution {
         int sum = 0;
         int leftsum = Integer.MIN_VALUE;
         int rightsum = Integer.MIN_VALUE;
+
+        // Maximum sum on the left side
         for (int i = mid; i >= start; i--) {
-            sum = sum + nums[i];
-            if (sum > leftsum) {
-                leftsum = sum;
-            }
+            sum += nums[i];
+            leftsum = Math.max(leftsum, sum);
         }
 
         sum = 0;
+
+        // Maximum sum on the right side
         for (int i = mid + 1; i <= end; i++) {
-            sum = sum + nums[i];
-            if (sum > rightsum) {
-                rightsum = sum;
-            }
+            sum += nums[i];
+            rightsum = Math.max(rightsum, sum);
         }
 
-        int totalsum = leftsum + rightsum;
-        return totalsum;
+        return leftsum + rightsum;
     }
 
     public int maxsubarray(int start, int end, int[] nums) {
-
         if (start == end) {
             return nums[start];
         }
 
-        int mid = (start + end) / 2;
+        int mid = start + (end - start) / 2;
 
         int leftSum = maxsubarray(start, mid, nums);
         int rightSum = maxsubarray(mid + 1, end, nums);
-        int totalSum = CrossSUM(start, end, mid, nums);
+        int crossSum = CrossSUM(start, end, mid, nums);
 
-        if (leftSum > rightSum) {
-            if (leftSum > totalSum) {
-                return leftSum;
-            } else {
-                return totalSum;
-            }
-        } else {
-            if (rightSum > totalSum) {
-                return rightSum;
-            } else {
-                return totalSum;
-            }
-        }
+        return Math.max(crossSum, Math.max(leftSum, rightSum));
     }
 
     public int maxSubArray(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
-
-        return maxsubarray(start, end, nums);
+        return maxsubarray(0, nums.length - 1, nums);
     }
 }
