912b666a5a74860ecb7b80e60f2834e52429f571
SankalpVerma31 <mrblackcap31@gmail.com>
2026-08-14 10:03:36 +0530

    Time: 1 ms (89.42%), Space: 43.1 MB (78.86%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int upper=0;
+        for(int i =0;i<word.length();i++){
+            if (Character.isUpperCase(word.charAt(i))) {
+                upper++;
+            }
+        }
+        if(upper==0){
+            return true;
+        }
+        else if(upper==word.length()){
+            return true;
+        }
+        else if (upper==1&&Character.isUpperCase(word.charAt(0))) {
+            return true;
+        }
+        return false;
+    }
+}
